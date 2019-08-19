527aa34b1b48fe64398e669147818e2f803e443c
btian <btian@hpe.com>
2019-08-19 18:12:54 +0800

    Update1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input id="select_pic" type="file" v-on:change="onInputFileChange">
+    <div class="preview">
+      <img id="image" v-bind:src="img_src" v-on:load="onImgload"/>
+      <p id="info" v-text="img_info"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
+// import { MCClient, Status, Result  } from '../../lib/ws-mc/mc-client-service'
+// import { ConnectionProperties, Address, Credentials, Protocol } from '../../lib/ws-mc/connection-properties'
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  //@Prop() private mcUrl: string = 'input the MC server url';
+  // @Prop() private response: string = '';
+  @Prop() private file = undefined;
+  /**
+   * name
+   */
+  // @Prop() private input = document.querySelector("input")
+  // @Prop() private preview = document.querySelector('.preview');
+
+  @Prop() private fileTypes = [
+    'image/jpeg',
+    'image/pjpeg',
+    'image/png'
+  ];
+
+  @Prop() private width = -1;
+  @Prop() private height = -1;
+  @Prop() private curInput = undefined;
+  @Prop() private curImg = undefined;
+  @Prop() private curImgInfo = undefined;
+
+  data: {
+    img_src: '';
+    img_info: ''
+  }
+
+  public beforeMount(){
+    this.curInput = document.querySelector("#select_pic");
+    this.curImg = document.querySelector('#image');
+    this.curImgInfo = document.querySelector('#info');
+
+    this.curImg.onImgload = function(){
+      this.width = this.curImg.width;
+      this.height = this.curImg.height;
+    };
+  }
+  
+  public validFileType (file) {
+    for(var i = 0; i < this.fileTypes.length; i++) {
+      if(file.type === this.fileTypes[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public returnFileSize(number) {
+    if(number < 1024) {
+      return number + 'bytes';
+    } else if(number > 1024 && number < 1048576) {
+      return (number/1024).toFixed(1) + 'KB';
+    } else if(number > 1048576) {
+      return (number/1048576).toFixed(1) + 'MB';
+    }
+  }
+
+  public onInputFileChange (e) {
+    var preview = document.querySelector('.preview');
+    var curFiles = this.curInput.files;
+    if(curFiles.length != 0) {
+      //var list = document.createElement('ol');
+      // preview.appendChild(list);
+      for(var i = 0; i < curFiles.length; i++) {
+        var imageItem = document.createElement('li');
+        // var para = document.createElement('p');
+        if(this.validFileType(curFiles[i])) {
+          this.curImgInfo.textContent = 'File name ' + curFiles[i].name + ', file size ' + this.returnFileSize(curFiles[i].size) + '.';
+
+          this.curImg.src = window.URL.createObjectURL(curFiles[i]);
+        }
+      }
+    }
+  }
+
+
+
+    // this.mcClient = new MCClient(this.mcUrl)
+    // var result:Result = this.mcClient.login(connectionPropertiesMock)
 }
 </script>
 
@@ -55,5 +111,12 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.box__dragndrop,
+.box__uploading,
+.box__success,
+.box__error {
+  display: none;
 }
 </style>
